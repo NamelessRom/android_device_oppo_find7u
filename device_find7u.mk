924d813b4bd0c609d3112db168d9e44b19895db2
Steve Kondik <shade@chemlab.org>
2014-07-30 16:20:03 -0400

    msm8974: Offload all the things!
     * Offload is very stable now, so offload all the things.
    
    Change-Id: I41cb0df2310e9c15ad5c434858c65b26169911f4

--- device_find7u.mk
+++ device_find7u.mk
@@ -93,11 +93,13 @@ PRODUCT_PROPERTY_OVERRIDES += \
     media.aac_51_output_enabled=true \
     audio.offload.pcm.enable=true \
     audio.offload.24bit.enable=1 \
-    qemu.hw.mainkeys=1\
-    view.scroll_friction=1\
-    ro.min_pointer_dur=8\
-    ro.min_fling_velocity=8000\
-    ro.max_fling_velocity=16000
+    qemu.hw.mainkeys=1 \
+    view.scroll_friction=1 \
+    ro.min_pointer_dur=8 \
+    ro.min_fling_velocity=8000 \
+    ro.max_fling_velocity=16000 \
+    audio.offload.min.duration.secs=0
+
 
 # Camera
 PRODUCT_PACKAGES += \
