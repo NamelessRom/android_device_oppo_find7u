35b53d1c1f84313c100376f74c8f1d091ea15a27
jumoog <kilianvonpflugk@gmail.com>
2014-08-10 19:15:11 +0200

    use google gcc
    Change-Id: I29729fd3cef2b6e599618d9fe800677ef1e6bc52

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -116,7 +116,6 @@ TARGET_PROVIDES_LIBLIGHT := true
 # Kernel
 TARGET_KERNEL_SOURCE := kernel/oppo/find7
 TARGET_KERNEL_CONFIG := custom_find7_defconfig
-TARGET_GCC_VERSION_EXP := 4.8
 BOARD_CUSTOM_BOOTIMG_MK := device/oppo/find7u/mkbootimg.mk
 BOARD_KERNEL_BASE        := 0x00000000
 BOARD_KERNEL_PAGESIZE    := 2048
