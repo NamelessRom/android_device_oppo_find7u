2bfa5154872247c6357bdf33b19be4a7c2c77353
jumoog <kilianvonpflugk@gmail.com>
2014-08-15 23:04:51 +0200

    mv some stuff to android_device_oppo_msm8974-common
    Change-Id: I5dc0966cb04295929758be9eab3359c62d827cc6
    
    cleanup dependencies
    
    Change-Id: I20a84a22f54718cfc0686662fe083d7d740023a2
    
    derp derp
    
    Change-Id: Ib27505920effb225f0a237d39de307ccef06a55f
    
    add missing system.prop
    
    Change-Id: I2d954df2d8203be657434ccb946426d91b4ff857
    
    more cleanup
    
    Change-Id: Ibda0addcb61774d8c0e23a8a2b56e4dce8248767
    
    fix unified sdcard access
    
    Change-Id: Ia8d4b21019862d52fec7bab023dfc910590109e7

--- nameless_find7u.mk
+++ nameless_find7u.mk
@@ -3,21 +3,11 @@ $(call inherit-product, device/oppo/find7u/full_find7u.mk)
 # Get the prebuilt list of APNs
 $(call inherit-product, vendor/nameless/config/apns.mk)
 
-# Inherit from the common Open Source product configuration
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-
 # Inherit from our custom product configuration
 $(call inherit-product, vendor/nameless/config/common.mk)
 
 # Enhanced NFC
 $(call inherit-product, vendor/nameless/config/nfc_enhanced.mk)
 
-# Inline kernel
-TARGET_KERNEL_SOURCE := kernel/oppo/find7
-TARGET_KERNEL_CONFIG := custom_find7_defconfig
-
 PRODUCT_NAME := nameless_find7u
 PRODUCT_DEVICE := find7u
-PRODUCT_BRAND := Oppo
-PRODUCT_MODEL := Find7
-PRODUCT_MANUFACTURER := Oppo
