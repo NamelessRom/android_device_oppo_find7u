0379f0ab96a021dd8868dcf72f81182fa0740e54
jumoog <kilianvonpflugk@gmail.com>
2014-10-27 09:50:21 +0100

    split tree keep find7u
    Change-Id: I239626a33f26506ddfcea43d904db4868129922e

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -17,17 +17,8 @@
 # Inherit from MSM8974 common
 -include device/oppo/msm8974-common/BoardConfigCommon.mk
 
-# Kernel
-TARGET_KERNEL_CONFIG := custom_find7_defconfig
-BOARD_CUSTOM_BOOTIMG_MK := device/oppo/find7u/mkbootimg.mk
-BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x3F ehci-hcd.park=3
-
-# Bluetooth
-BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/oppo/find7u/bluetooth
-
-# Camera
-USE_DEVICE_SPECIFIC_CAMERA := true
-COMMON_GLOBAL_CFLAGS += -DOPPO_CAMERA_HARDWARE
+# Inherit from Find7 common
+-include device/oppo/find7-common/BoardConfigCommon.mk
 
 # Filesystem
 BOARD_BOOTIMAGE_PARTITION_SIZE     := 16777216
@@ -38,21 +29,14 @@ BOARD_SYSTEMIMAGE_PARTITION_SIZE   := 1364320256
 BOARD_USERDATAIMAGE_PARTITION_SIZE := 13271448576
 #BOARD_USERDATAIMAGE_PARTITION_SIZE := 3221225472
 
-# Init
-TARGET_INIT_VENDOR_LIB := libinit_find7
-
 # Workaround for factory issue
 BOARD_VOLD_CRYPTFS_MIGRATE := true
 
-# Recovery
 TARGET_RECOVERY_FSTAB := device/oppo/find7u/rootdir/etc/fstab.qcom
 RECOVERY_VARIANT      := philz
 
 # Assert
-TARGET_OTA_ASSERT_DEVICE := find7,find7a,find7u,X9007,X9006,FIND7
-
-# Releasetools
-TARGET_RELEASETOOLS_EXTENSIONS := device/oppo/find7u
+TARGET_OTA_ASSERT_DEVICE := find7,find7s,find7u
 
 # inherit from the proprietary version
 -include vendor/oppo/find7/BoardConfigVendor.mk
